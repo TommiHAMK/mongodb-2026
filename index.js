797cc7cd30fe729c69d74ce360f0e41956719fd1
tommiHAMK <tommi.saksa@hamk.fi>
2026-04-10 10:33:34 +0300

    Testing MongoDb done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const uri = process.env.URI;
 
-//promise
+//promise 
 mongoose.connect(uri)
 .then((result) => console.log("Connected to dB"))
 .catch((err) => console.log(err))
@@ -14,13 +14,28 @@ mongoose.connect(uri)
 const Product = require('./models/Product');
 
 const product = new Product({
-    name: "Car",
-    price: 1500
+    name: "Bike",
+    price: 500
 });
 
 // Remember to set the default database to the connection string
 
 //product.save();
+
+
+const saveProduct = async (product) => {
+    try 
+    {
+        const result = await product.save();
+        console.log(result);
+    }
+    catch (err)
+    {
+        console.log(err);
+    }
+}
+
+saveProduct(product);
 
 Product.find()
 .then((result) => {
