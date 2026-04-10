432b3263fcd6b38d9ac0b7bff39f124d1ff9c627
tommiHAMK <tommi.saksa@hamk.fi>
2026-04-10 10:01:11 +0300

    Hard coded add product and find all

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-console.log(process.env.SECRETINFO);
+// console.log(process.env.SECRETINFO);
 
 const uri = process.env.URI;
-
 
 //promise
 mongoose.connect(uri)
 .then((result) => console.log("Connected to dB"))
 .catch((err) => console.log(err))
 
+// importing the schema
+const Product = require('./models/Product');
 
+const product = new Product({
+    name: "Car",
+    price: 1500
+});
+
+// Remember to set the default database to the connection string
+
+//product.save();
+
+Product.find()
+.then((result) => {
+    console.log(result);
+})
+.catch((err) => {
+    console.log(err);
+});
